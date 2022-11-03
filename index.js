70a5bcd8176c66a98399e981b8012c3c90ac51b1
touhami-ben <brooklyn.ben76@gmail.com>
2022-11-03 12:28:26 -0400

    06-function

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ function findById(movies, id) {
     }
   }
   return null;
-  
+
 }
 
 /**
@@ -147,7 +147,15 @@ function findById(movies, id) {
  *  filterByGenre(movies, "Horror")
  *  //> []
  */
-function filterByGenre() {}
+function filterByGenre(movies, genre) {
+  let moviesByGenre = [];
+  for(let movie of movies){
+    if(movie.genre.toLowerCase().includes(genre.toLowerCase())){
+      moviesByGenre.push(movie)
+    }
+  }
+  return moviesByGenre;
+}
 
 /**
  * getAllMoviesReleasedAtOrBeforeYear()
